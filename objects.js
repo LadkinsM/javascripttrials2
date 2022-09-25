8085312e21891cb4ac445914b99dd565fab4907e
Mauri Adkins <mauriadkins@gmail.com>
2022-09-24 19:27:28 -0500

    Objects.js Part 1 Completed

--- objects.js
+++ objects.js
@@ -2,8 +2,21 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCounts = {}
+
+  const wordsList = phrase.split(' ')
+  
+  for (const word of wordsList) {
+    if (!wordCounts.word) {
+      wordCounts[word] = 1;
+    } else {
+      wordCounts[word] += 1;
+    }
+  }
+  return wordCounts
 }
+
+console.log(countWords('Hello There'))
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
